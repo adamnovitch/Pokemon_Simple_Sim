38b5660d36d70ebcab3f4794d058a132f1ba00aa
Adam Novitch <adam.novitch@gmail.com>
2017-02-19 14:06:24 -0500

    Introduce statdef.h
    statdef.h includes defines for stats (HP, ATTACK, etc),
    and also defines Physical/Special/Status move types.

---
+++ statdef.h
@@ -0,0 +1,12 @@
+#define HP         0
+#define ATTACK     1
+#define DEFENSE    2
+#define SP_ATTACK  3
+#define SP_DEFENSE 4
+#define SPEED      5
+#define ACCURACY   6
+#define EVADE      7
+
+#define PHYSICAL   0
+#define SPECIAL    1
+#define STATUS     2
